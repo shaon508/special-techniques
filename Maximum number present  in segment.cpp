f7edda446db80fb1c4b33a759b25f6635930d9b9
Shaon Roy <113965094+shaon508@users.noreply.github.com>
2024-01-04 23:00:19 +0600

    Update Maximum number present  in segment.cpp

--- Maximum number present  in segment.cpp
+++ Maximum number present  in segment.cpp
@@ -1,1 +1,34 @@
-
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long int
+#define ff first
+#define ss second
+#define fast ios_base::sync_with_stdio(false);cin.tie(NULL);
+int main()
+{
+ 
+    fast;
+    int TC=1;
+    while(TC--)
+    {
+        ll n,a,b,maxx=0,one=0;
+        cin>>n;
+        pair<ll,ll>p[2*n];
+        for(int i=0; i<n; i++)
+        {
+            cin>>a>>b;
+            p[i].ff=a;
+            p[i].ss=1;
+            p[n+i].ff=b;
+            p[n+i].ss=-1;
+        }
+        sort(p,p+2*n);
+        for(int i=0; i<2*n; i++)
+        {
+            one+=p[i].ss;
+            maxx=max(one,maxx);
+        }
+        cout<<maxx<<endl;
+    }
+    return 0;
+}
